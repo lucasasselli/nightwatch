37c6dfbdc02c27f374383f6d93a8065d4ad09895
Luca Sasselli <lucasasselli93@gmail.com>
2024-05-25 15:32:56 +0100

    Code cleanup

--- libs/minigl/include/minigl.h
+++ libs/minigl/include/minigl.h
@@ -5,13 +5,18 @@
 #include "pd_api.h"
 #include "texture.h"
 
+#ifndef SCREEN_SIZE_X
 #define SCREEN_SIZE_X 400
+#endif
+
+#ifndef SCREEN_SIZE_Y
 #define SCREEN_SIZE_Y 240
+#endif
 
 #define MINIGL_INLINE static inline __attribute((always_inline))
 
-#define MINIGL_SCANLINE
-#define MINIGL_PERSP_CORRECT
+// #define MINIGL_SCANLINE
+// #define MINIGL_PERSP_CORRECT
 
 // TODO: Make the library plaform agnostic
 extern PlaydateAPI* pd;
@@ -20,12 +25,28 @@ extern PlaydateAPI* pd;
 extern uint8_t c_buff[SCREEN_SIZE_Y * SCREEN_SIZE_X];
 extern float z_buff[SCREEN_SIZE_Y * SCREEN_SIZE_X];
 
+typedef enum {
+    PERF_CLIP,
+    PERF_CULL,
+    PERF_POLY,
+    PERF_FRAG
+} minigl_perf_event_t;
+
+typedef union {
+    int array[4];
+    struct {
+        int clip;
+        int cull;
+        int poly;
+        int frag;
+    };
+} minigl_perf_data_t;
+
 // State objects
 typedef struct {
     minigl_tex_t texture;
     minigl_tex_mode_t texture_mode;
     minigl_tex_t dither;
-    minigl_dither_mode_t dither_mode;
     minigl_face_cull_t face_cull;
     uint8_t draw_color;
 } minigl_cfg_t;
@@ -36,12 +57,10 @@ void minigl_set_dither(minigl_tex_t t);
 
 void minigl_set_color(uint8_t color);
 
-void minigl_perf_print(void);
+minigl_perf_data_t minigl_perf_get(void);
 
 void minigl_perf_clear(void);
 
 void minigl_clear(uint8_t color, int depth);
 
 void minigl_draw(minigl_obj_buf_t obj);
-
-int minigl_frame_to_file(char* path);
